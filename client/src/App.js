e84105398db99c3611faee60b1dccc9b7d32794f
Sharon Caphona Devanayagam <sharondeva197@gmail.com>
2025-03-30 17:49:10 +0530

    Finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,64 @@
-import React from 'react';
-import Tasks from './Tasks';
-import {Paper, TextField} from '@material-ui/core';
-import {Checkbox, Button} from '@material-ui/core';
-import './App.css';
+import React from "react";
+import Tasks from "./Tasks";
+import { Paper, TextField } from "@material-ui/core";
+import { Checkbox, Button } from "@material-ui/core";
+import "./App.css";
 
 class App extends Tasks {
-  state = { tasks: [], currentTask: ""} 
-  render() { 
-    return ();
+  state = { tasks: [], currentTask: "" };
+  render() {
+    const { tasks } = this.state;
+    return (
+      <div className="App flex">
+        <Paper elevation={3} className="container">
+          <div className="heading">To-Do</div>
+          <form
+            onSubmit={this.handleSubmit}
+            className="flex"
+            style={{ margin: "15px 0" }}
+          >
+            <TextField
+              variant="outlined"
+              size="small"
+              style={{ width: "80px" }}
+              value={this.state.currentTask}
+              required={true}
+              onChange={this.handleChange}
+              placeholder="Add New To-Do"
+            />
+            <Button
+              style={{ height: "40px" }}
+              color="primary"
+              variant="outlined"
+              type="submit"
+            >
+              Add task
+            </Button>
+          </form>
+          <div>
+            {tasks.map((task) => (
+              <Paper key={task._id} className="flex task_container">
+                <Checkbox
+                  checked={task.completed}
+                  onClick={() => this.handleUpdate(task._id)}
+                  color="primary"
+                />
+                <div className={task.completed ? "task line_through" : "task"}>
+                  {task.task}
+                </div>
+                <Button
+                  onClick={() => this.handleDelete(task._id)}
+                  color="secondary"
+                >
+                  Delete
+                </Button>
+              </Paper>
+            ))}
+          </div>
+        </Paper>
+      </div>
+    );
   }
 }
- 
-export default ;
+
+export default App;
